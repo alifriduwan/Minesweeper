e3b3059c6bead1c6e758a843fa90ac6724c85887
alif <alifriduwan.a@gmail.com>
2023-02-07 21:10:10 +0700

    add files

---
+++ Screen/main.py
@@ -0,0 +1,60 @@
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+user_database = [
+    dict(username="admin", password="adminadmin"),
+    dict(username="Phoorin", password="3799"),
+]
+class PlayGameScreen(Screen):
+    def __init__(self, **kwargs):
+
+        super().__init__(**kwargs)
+        self.add_widget(Label(text="Go"))
+
+
+class LoginScreen(Screen):
+    def __init__(self, **kwargs):
+
+        super().__init__(**kwargs)
+        layout = BoxLayout(orientation="vertical")
+        layout.add_widget(Label(text="Login", font_size=50))
+
+        username_layout = BoxLayout()
+        username_layout.add_widget(Label(text="Username"))
+        self.username_input = TextInput(multiline=False)
+        username_layout.add_widget(self.username_input)
+        layout.add_widget(username_layout)
+
+        password_layout = BoxLayout()
+        password_layout.add_widget(Label(text="Password"))
+        self.password_input = TextInput(password=True, multiline=False)
+        password_layout.add_widget(self.password_input)
+        layout.add_widget(password_layout)
+
+        layout.add_widget(Button(text="Submit", on_press=self.on_login))
+        self.add_widget(layout)
+
+    def on_login(self, instance):
+        for user in user_database:
+            if (
+                user["username"] == self.username_input.text.strip()
+                and user["password"] == self.password_input.text
+            ):
+                print("Hello", self.username_input.text)
+                self.manager.current = "play_game"
+                break
+
+class HelloWorldApp(App):
+    def build(self):
+        self.sm = ScreenManager()
+        self.sm.add_widget(LoginScreen(name="login"))
+        self.sm.add_widget(PlayGameScreen(name="play_game"))
+        return self.sm
+
+if __name__ == "__main__":
+    HelloWorldApp().run()
